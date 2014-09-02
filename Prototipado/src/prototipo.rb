a205c5977b601d3df73b152e228c1f6a8778cc48
Juan Pablo Jacob <juampejacob@hotmail.com>
2014-09-01 23:17:32 -0300

    El prototipo agrega variables y genera las propiedades de la variable

--- Prototipado/src/prototipo.rb
+++ Prototipado/src/prototipo.rb
@@ -1,2 +1,20 @@
-class Prototipo
+module Prototipo
+
+  def agregar_variable(selector, valor)
+    self.instance_variable_set(selector, valor)
+    nombre = selector.to_s.delete '@'
+    self.class.instance_eval do
+        attr_accessor nombre
+    end
+  end
+
+  def cantidad_variables
+    self.instance_variables.count
+  end
+
+end
+
+class Guerrero
+  include Prototipo
+
 end
--- Prototipado/test/prototipo_spec.rb
+++ Prototipado/test/prototipo_spec.rb
@@ -1,9 +1,15 @@
+load '../src/prototipo.rb'
+
 require 'rspec'
 
-describe 'Test Prototipado' do
+describe 'Prototipado - Ruby' do
 
-  it 'la suma anda' do
-    expect(23 + 17).to eq 40
+  it 'Agregar una variable' do
+    conan = Guerrero.new
+    conan.agregar_variable(:@vida, 100)
+    expect(conan.cantidad_variables).to eq 1 # testea que se agregue la variable
+    conan.vida = 200
+    expect(conan.vida).to eq 200 # testea el que se generen la propiedades
   end
 
 end
